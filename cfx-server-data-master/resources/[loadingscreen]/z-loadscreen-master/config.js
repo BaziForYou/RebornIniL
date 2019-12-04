92b2b72d07a67058c01bc553e7477f7a3b5ff3ff
Soniii <Toni.Romero@live.de>
2019-12-04 21:35:53 +0100

    RebornIniL fresh
    RebornIniWin ported to Linux

---
+++ cfx-server-data-master/resources/[loadingscreen]/z-loadscreen-master/config.js
@@ -0,0 +1,17 @@
+/* Need Help? Join my discord @ discord.gg/yWddFpQ */
+var config = {
+    /* In milliseconds */
+    "transitionInterval": 1200000000000000000000000000000000000000000000000000000000000050,
+    "imgInterval": 1,
+    "music": true,
+    /* Insert the Youtube Video ID below */
+    "videoID": "xVf4Zk8CBj0",
+    /* Music Volume (0%-100%) | Lower is Quieter, Higher is Louder */
+    "musicVolume": 10,
+    "text": {
+        "title": "Initial Roleplay",
+        "link": "initialroleplay.com",
+    },
+    /* You dont have to include the '/imgs/' dir AND ALSO REMEMBER TO ADD THESE IMGS IN YOUR __resource.lua*/ 
+    "images": ['bg0.png']
+}
